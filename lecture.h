37f3801c78c17e04423a28be55aee076bba73623
DelaunayCyprien <cyprien.delaunay@gmail.com>
2025-06-13 15:00:14 +0200

    Add files via upload

--- lecture.h
+++ lecture.h
@@ -1,16 +1,16 @@
-//
-// Created by Noé Gavard on 12/06/2025.
-//
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 
 #ifndef LECTURE_H
 #define LECTURE_H
 
 #define LINE_SIZE 2000
 
-void startSect(char* sect_name);
+int genererFichiersHTML_Simple(int nombre);
+void generer_index();
+
 
 #endif //LECTURE_H
